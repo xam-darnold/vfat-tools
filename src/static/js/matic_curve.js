d90fab560d63a54b10cbf503ae0b1683d00e57e0
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-04-21 15:28:24 +0300

    curve update

--- src/static/js/matic_curve.js
+++ src/static/js/matic_curve.js
@@ -13,36 +13,48 @@ async function main() {
   const tokens = {};
   const prices = await getMaticPrices();
 
-
+  await getCurveToken(App);
   
   hideLoading();
 }
 
-async function getCurveToken(App, tokenAddress){
-  const crv = new ethers.Contract(tokenAddress, CURVE_ABI, App.provider);
+async function getCurveToken(App) {
   const minterAddress = "0x445FE580eF8d70FF569aB36e80c647af338db351"
-  const res = await createCurveToken(App, crv, tokenAddress, minterAddress);
-  window.localStorage.setItem(tokenAddress, "Curve");
-  return res;
+  const tokenAddress = "0xE7a24EF0C5e95Ffb0f6684b813A78F2a3AD7D171"
+  const crv = new ethers.Contract(tokenAddress, CURVE_ABI, App.provider);
+  const minter = new ethers.Contract(minterAddress, MINTER_ABI, App.provider)
+  //const virtualPrice = await minter.get_virtual_price();
+  const coin0 = await minter.coins(0);
+  const coin1 = await minter.coins(1);
+  const coin2 = await minter.coins(2);
+  const token0 = await getMaticToken(App, coin0, minterAddress);
+  const token1 = await getMaticToken(App, coin1, minterAddress);
+  const token2 = await getMaticToken(App, coin2, minterAddress);
+  const decimals = await crv.decimals();
+  //const symbol = await crv.symbol();
+  //const usersTotalBalance = await crv.balanceOf(App.YOUR_ADDRESS) / 10 ** decimals
+  const totalCurveStaked = token0.staked + token1.staked + token2.staked;
+  const pct0 = token0.staked / totalCurveStaked * 100
+  const pct1 = token1.staked / totalCurveStaked * 100
+  const pct2 = token2.staked / totalCurveStaked * 100
+  //_print(`${symbol}`);
+ // _print(`Users Balance ${usersTotalBalance}`);
+  _print(`${token0.symbol} ${formatMoney(token0.staked)} (${pct0.toFixed(2)}%)`);
+  _print(`${token1.symbol} ${formatMoney(token1.staked)} (${pct1.toFixed(2)}%)`);
+  _print(`${token2.symbol} ${formatMoney(token2.staked)} (${pct2.toFixed(2)}%)`);
+  _print(`${token0.symbol}+${token1.symbol}+${token2.symbol}: ${formatMoney(totalCurveStaked)}`);
 }
 
-async function createCurveToken(App, curve, address, minterAddress) {
-  const minter = new ethers.Contract(minterAddress, MINTER_ABI, App.provider)
-  const virtualPrice = await minter.get_virtual_price();
-  const coin0 = await minter.coins(0);
-  const token = await getMaticToken(App, coin0, address);
-  const decimals = await curve.decimals();
-  const usersBalance = await curve.balanceOf(App.YOUR_ADDRESS)
-  return {
-      address,
-      name : await curve.name(),
-      symbol : await curve.symbol(),
-      totalSupply : await curve.totalSupply(),
-      decimals : decimals,
-      usersBalance: usersBalance  / 10 ** decimals,
-      contract: curve,
-      tokens : [address, coin0],
-      token,
-      virtualPrice : virtualPrice / 1e18
-  };
-}
+/*return {
+  address,
+  name,
+  symbol,
+  totalSupply,
+  decimals : decimals,
+  staked:  staked / 10 ** decimals,
+  unstaked: unstaked  / 10 ** decimals,
+  contract: curve,
+  tokens : [address, coin0],
+  token,
+  virtualPrice : virtualPrice / 1e18
+};*/
